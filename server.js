c102914dbec0740f3b0bd4ba4125af20ff8fdc2f
1egoman <rsg1egoman@gmail.com>
2016-08-07 21:30:30 -0400

    code: add density shortcut

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.set('view engine', 'ejs');
 // For this to work, you need to make the sheet public with File -> Publish to the Web
 // This means no api key is needed (no rate limit) and auth sucks to use in the googl ecosystem.
 app.get('/', (req, res) => res.redirect('/sheets/1DnHlU9IAN5-GRj3UXCnePmT02Fl2xD7fbvCx1uLKzeM'));
+app.get('/density', (req, res) => res.redirect('/sheets/1frVwofgRgZYBo5NCGnPV2eNQB8gKvLEon21T6TGG5b0'));
 
 app.get('/sheets/:sheetId', (req, res) => {
   if (req.query.pick) {
